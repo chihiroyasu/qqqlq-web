ee39fecebf716914dbd6768486abba9f6b621b96
Yasu <ychihiro0315@icloud.com>
2025-08-20 18:56:40 +0900

    chakra uiの導入とCard Componentを作ってみた

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, VStack } from '@chakra-ui/react'
+import CardDemo from './components/ui/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container 
+      maxW="80%"  // 幅を80%に設定
+      mx="auto"     // 中央寄せ
+      p={4}
+    >
+      <VStack>
+        <CardDemo />
+      </VStack>
+    </Container>
   )
 }
 
-export default App
+export default App;
